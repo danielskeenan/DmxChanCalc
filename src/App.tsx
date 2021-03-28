fb07fb0ba8cdd2b3f056d34b5ce47c1a9d205b94
Dan Keenan <dk@dankeenan.org>
2021-03-28 12:51:32 -0400

    Teach form about percentages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent, useState} from 'react';
-import {Col, Container, Form, Navbar, Row} from "react-bootstrap";
+import {Col, Container, Form, InputGroup, Navbar, Row} from "react-bootstrap";
 import './style.scss';
 
 export default function App() {
@@ -16,41 +16,127 @@ export default function App() {
                     <Form.Group as={Row}>
                         <Form.Label column sm={1} htmlFor="chan-a">Chan A</Form.Label>
                         <Col>
-                            <NumberInput name="chan-a"
-                                         value={chanA}
-                                         onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e.target.value, 255, setChanA)}
-                            />
+                            <InputGroup className="mb-2">
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text>DMX</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <Form.Control type="number"
+                                              name="chan-a"
+                                              value={chanA}
+                                              min="0"
+                                              max="255"
+                                              onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e.target.value, 255, setChanA)}
+                                />
+                            </InputGroup>
+                        </Col>
+                        <Col>
+                            <InputGroup className="mb-2">
+                                <Form.Control type="number"
+                                              name="chan-a-pct"
+                                              value={Math.round(chanA / 255 * 100)}
+                                              min="0"
+                                              max="100"
+                                              onChange={(e: ChangeEvent<HTMLInputElement>) => handlePercentChange(e.target.value, 255, setChanA)}
+                                />
+                                <InputGroup.Append>
+                                    <InputGroup.Text>%</InputGroup.Text>
+                                </InputGroup.Append>
+                            </InputGroup>
                         </Col>
                     </Form.Group>
                     <Form.Group as={Row}>
                         <Form.Label column sm={1} htmlFor="chan-b">Chan B</Form.Label>
                         <Col>
-                            <NumberInput name="chan-b"
-                                         value={chanB}
-                                         onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e.target.value, 255, setChanB)}
-                            />
+                            <InputGroup className="mb-2">
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text>DMX</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <Form.Control type="number"
+                                              name="chan-b"
+                                              value={chanB}
+                                              min="0"
+                                              max="255"
+                                              onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e.target.value, 255, setChanB)}
+                                />
+                            </InputGroup>
+                        </Col>
+                        <Col>
+                            <InputGroup className="mb-2">
+                                <Form.Control type="number"
+                                              name="chan-b-pct"
+                                              value={Math.round(chanB / 255 * 100)}
+                                              min="0"
+                                              max="100"
+                                              onChange={(e: ChangeEvent<HTMLInputElement>) => handlePercentChange(e.target.value, 255, setChanB)}
+                                />
+                                <InputGroup.Append>
+                                    <InputGroup.Text>%</InputGroup.Text>
+                                </InputGroup.Append>
+                            </InputGroup>
                         </Col>
                     </Form.Group>
                     <hr/>
                     <Form.Group as={Row}>
                         <Form.Label column sm={1} htmlFor="absolute">16-Bit</Form.Label>
                         <Col>
-                            <NumberInput value={(chanA << 8) + chanB}
-                                         max="65535"
-                                         onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                                             const val = parseInt(e.target.value);
-                                             handleChange(val, 65535, (newVal => {
-                                                 setChanA((newVal & 0xFF00) >> 8);
-                                                 setChanB(newVal & 0xFF);
-                                             }));
-                                         }}
-                            />
+                            <InputGroup className="mb-2">
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text>DMX</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <Form.Control type="number"
+                                              name="value"
+                                              value={(chanA << 8) + chanB}
+                                              min="0"
+                                              max="65535"
+                                              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                                  const val = parseInt(e.target.value);
+                                                  handleChange(val, 65535, (newVal => {
+                                                      setChanA((newVal & 0xFF00) >> 8);
+                                                      setChanB(newVal & 0xFF);
+                                                  }));
+                                              }}
+                                />
+                            </InputGroup>
+                        </Col>
+                        <Col>
+                            <InputGroup className="mb-2">
+                                <Form.Control type="number"
+                                              name="value-pct"
+                                              value={Math.round(((chanA << 8) + chanB) / 65535 * 100)}
+                                              min="0"
+                                              max="100"
+                                              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                                  const val = parseInt(e.target.value);
+                                                  handlePercentChange(val, 65535, (newVal => {
+                                                      setChanA((newVal & 0xFF00) >> 8);
+                                                      setChanB(newVal & 0xFF);
+                                                  }));
+                                              }}
+                                />
+                                <InputGroup.Append>
+                                    <InputGroup.Text>%</InputGroup.Text>
+                                </InputGroup.Append>
+                            </InputGroup>
                         </Col>
                     </Form.Group>
                 </Form>
             </Container>
         </>
     );
+}
+
+function handlePercentChange(value: string | number, max: number, setter: (newVal: number) => void) {
+    let intVal = value;
+    if (typeof intVal !== 'number') {
+        intVal = parseInt(intVal);
+    }
+
+    // Validate
+    if (isNaN(intVal) || intVal < 0 || intVal > max) {
+        return;
+    }
+
+    handleChange(Math.floor(intVal / 100 * max), max, setter);
 }
 
 function handleChange(value: string | number, max: number, setter: (newVal: number) => void) {
@@ -64,15 +150,4 @@ function handleChange(value: string | number, max: number, setter: (newVal: numb
         return;
     }
     setter(intVal);
-}
-
-function NumberInput(props: Record<string, any>) {
-    return (
-        <Form.Control type="number"
-                      min="0"
-                      max="255"
-                      className="mb-2"
-                      {...props}
-        />
-    );
 }
